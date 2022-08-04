3014c246a0c90be0e1ed96b2fc3da13f267d63ea
Felix Titov <titow.feliks@yandex.ru>
2022-08-04 14:31:59 +0300

    Initial Commit

---
+++ ArchitectureApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ArchitectureApp
+//
+//  Created by Felix Titov on 8/4/22.
+//  Copyright © 2022 by Felix Titov. All rights reserved.
+//  
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
